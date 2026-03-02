316aaceab519189351e055063775c37d9edc721d
NejChoco <171265952+NejChoco@users.noreply.github.com>
2026-03-03 00:00:54 +0800

    update: new act

---
+++ Activities/bidning-act.py
@@ -0,0 +1,19 @@
+print ("\n-----number 1-----")
+# Right side binding
+print (20 + 30 * 5)
+
+print ("\n-----number 2-----")
+# Left side binding
+print(40/4//2)
+
+print("\n-----number 3-----")
+# Right side binding
+print(5 ** 2 ** 2)
+
+print("\n-----number 4-----")
+# Right side binding
+print(100+30-50)
+
+print("\n-----number 5-----")
+# Left side binding 
+print(168 % 70 // 8)
