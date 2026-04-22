5eece8634747c3ccc09d09baaeaed33ee84475b7
NejChoco <171265952+NejChoco@users.noreply.github.com>
2026-04-23 01:32:17 +0800

    code now aligns with the requirements of the peta

--- Activities/progLang06TP1.py
+++ Activities/progLang06TP1.py
@@ -1,19 +1,11 @@
-"""
-
-print("====================================================")
-predefined_budget = float(input("predefined budget: ")) 
-print("====================================================")
-venue = float(input("venue: "))
-catering = float(input("catering: "))
-decoration = float(input("decoration: "))
-entertainment=float(input("entertainment: "))
-miscellaneous=float(input("miscellaneous: "))
-print("====================================================")
-
 def calculate_total_cost(venue, catering, decoration, entertainment, miscellaneous):
-    return venue + catering + decoration + entertainment + miscellaneous
-
-total_cost = calculate_total_cost(venue, catering, decoration, entertainment, miscellaneous)    
+    total_cost = 0
+    total_cost += venue
+    total_cost += catering
+    total_cost += decoration
+    total_cost += entertainment
+    total_cost += miscellaneous
+    return total_cost
 
 def check_budget(total_cost, predefined_budget):
     if total_cost > predefined_budget:
@@ -22,22 +14,6 @@ def check_budget(total_cost, predefined_budget):
         return "Budget Not Exceeded"
     else:
         return "Budget Exactly Matched"
-
-
-print(f"Predefined Budget: {predefined_budget} \nTotal Cost: {total_cost}")
-print(f"Budget Verdict: {check_budget(total_cost, predefined_budget)}")
-"""
-def calculate_total_cost(venue, catering, decoration, entertainment, miscellaneous):
-    return venue + catering + decoration + entertainment + miscellaneous
-
-def check_budget(total_cost, predefined_budget):
-    if total_cost > predefined_budget:
-        return "Budget Exceeded"
-    elif total_cost < predefined_budget:
-        return "Budget Not Exceeded"
-    else:
-        return "Budget Exactly Matched"
-
 
 while True:
     try:
@@ -58,5 +34,11 @@ while True:
         print(f"Predefined Budget: {predefined_budget} \nTotal Cost: {total_cost}")
         print(f"Budget Verdict: {check_budget(total_cost, predefined_budget)}")
     except ValueError:
-        print("NUMBER NGA LANG DEBAAA")
-        
+        print("Invalid input. Please enter numerical number only.")
+    print("====================================================")
+    press = input("Continue? \nPress any key to continue \nOr press N to Cancel\n")
+    if press.strip().upper() == 'N' :
+        print("====================================================")
+        print("Thank you for using the budget plan")
+        break
+    print("====================================================")
