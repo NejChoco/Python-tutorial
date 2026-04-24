01857dfcba29b17e96f5f3c239be3a0cf0c7325e
NejChoco <171265952+NejChoco@users.noreply.github.com>
2026-04-24 22:32:48 +0800

    finisehd the tp

--- Activities/progLang06TP1.py
+++ Activities/progLang06TP1.py
@@ -1,8 +1,14 @@
-def calculate_total_cost(venue, catering, decoration, entertainment, miscellaneous):
+def get_positive_input(prompt):
+    value = float(input(prompt))
+    if value < 0:
+        raise ValueError("Negative numbers are not allowed.")
+    return value
+
+def calculate_total_cost(venue, catering, decorations, entertainment, miscellaneous):
     total_cost = 0
     total_cost += venue
     total_cost += catering
-    total_cost += decoration
+    total_cost += decorations
     total_cost += entertainment
     total_cost += miscellaneous
     return total_cost
@@ -19,25 +25,25 @@ while True:
     try:
         print("\nBUDGET PLAN\n")
         print("====================================================")
-        predefined_budget = float(input("predefined budget: ")) 
+        predefined_budget = get_positive_input("Predefined budget: ")
         print("====================================================")
-        venue = float(input("venue: "))
-        catering = float(input("catering: "))
-        decoration = float(input("decoration: "))
-        entertainment=float(input("entertainment: "))
-        miscellaneous=float(input("miscellaneous: "))
-        
-        
-        total_cost = calculate_total_cost(venue, catering, decoration, entertainment, miscellaneous) 
-        
+        venue = get_positive_input("Venue: ")
+        catering = get_positive_input("Catering: ")
+        decoration = get_positive_input("Decorations: ")
+        entertainment = get_positive_input("Entertainment: ")
+        miscellaneous = get_positive_input("Miscellaneous: ")
+
+        total_cost = calculate_total_cost(venue, catering, decoration, entertainment, miscellaneous)
+
         print("====================================================")
         print(f"Predefined Budget: {predefined_budget} \nTotal Cost: {total_cost}")
         print(f"Budget Verdict: {check_budget(total_cost, predefined_budget)}")
-    except ValueError:
-        print("Invalid input. Please enter numerical number only.")
+    except ValueError as e:
+        print(f"Invalid input: {e}. Please enter positive numerical numbers only.")
+        continue
     print("====================================================")
     press = input("Continue? \nPress any key to continue \nOr press N to Cancel\n")
-    if press.strip().upper() == 'N' :
+    if press.strip().upper() == 'N':
         print("====================================================")
         print("Thank you for using the budget plan")
         break
